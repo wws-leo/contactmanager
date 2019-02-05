43842ae035f3a8e278ba5c7522bb5fe55b1cfb45
wws-leo <leo@ww-s.co>
2019-02-05 17:04:32 +0100

    initial commit

---
+++ src/components/test/Test.js
@@ -0,0 +1,32 @@
+import React, { Component } from "react";
+
+class Test extends Component {
+  state = {
+    title: "",
+    body: ""
+  };
+  componentDidMount() {
+    console.log("slifh");
+    fetch("https://jsonplaceholder.typicode.com/posts/1")
+      .then(response => response.json())
+      .then(data => {
+        console.log(data);
+
+        this.setState({
+          title: data.title,
+          body: data.body
+        });
+      });
+  }
+  render() {
+    const { title, body } = this.state;
+    return (
+      <div>
+        <h1>{title}</h1>
+        <p>{body}</p>
+      </div>
+    );
+  }
+}
+
+export default Test;

34c89ac6826d2937fb96041f66895f4f87142600
wws-leo <leo@ww-s.co>
2019-02-05 17:07:39 +0100

    Updates

---
+++ precache-manifest.20d8b10f2510e60e226d77dedf13b59f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "72aa82900d090390d8dc",
+    "url": "/contactmanager/static/js/runtime~main.72aa8290.js"
+  },
+  {
+    "revision": "c3cdefbfdf31f541817a",
+    "url": "/contactmanager/static/js/main.c3cdefbf.chunk.js"
+  },
+  {
+    "revision": "8b82c8d6d785362554e0",
+    "url": "/contactmanager/static/js/1.8b82c8d6.chunk.js"
+  },
+  {
+    "revision": "c3cdefbfdf31f541817a",
+    "url": "/contactmanager/static/css/main.266afd20.chunk.css"
+  },
+  {
+    "revision": "8b82c8d6d785362554e0",
+    "url": "/contactmanager/static/css/1.c54ff341.chunk.css"
+  },
+  {
+    "revision": "392f048a6d165e110532bfeebcfea213",
+    "url": "/contactmanager/index.html"
+  }
+];
